1629004d398d190a075aa19f0ae88b49b34090f5
Peter Ketcham <peter.ketcham@gmail.com>
2015-06-15 16:42:58 -0500

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Peter Ketcham on 6/15/15.
+//  Copyright (c) 2015 Peter Ketcham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
