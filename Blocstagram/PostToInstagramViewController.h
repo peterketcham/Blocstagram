194584f4f50d40eaa8370e66df7677e7ec625308
Peter Ketcham <peter.ketcham@gmail.com>
2015-07-24 15:13:50 -0500

    Photo Filters and Sending to Instagram

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Peter Ketcham on 7/23/15.
+//  Copyright (c) 2015 Peter Ketcham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+- (instancetype)initWithImage:(UIImage *)sourceImage;
+@end
